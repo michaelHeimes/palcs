41a16a52f2be86eb62f773f53c62409bb1414bc2
Michael Heimes <michael@cairndigitalmedia.com>
2024-01-08 23:32:16 -0500

    dev

---
+++ inc/menu.php
@@ -0,0 +1,178 @@
+<?php
+// Register menus
+register_nav_menus(
+	array(
+		'social-links'	=> __( 'Social Links', 'trailhead' ),		// Social Nav
+		'header-secondary-nav'	=> __( 'Header Secondary Menu', 'trailhead' ),		// Social Nav
+		'header-nav'		=> __( 'The Header Menu', 'trailhead' ),		// Main nav in header
+		'offcanvas-nav'	=> __( 'The Off-Canvas Menu', 'trailhead' ),	// Off-Canvas nav
+		'offcanvas-supplemental-nav'	=> __( 'The Off-Canvas Supplemental Menu', 'trailhead' ),	// Off-Canvas nav
+		'footer-nav'	=> __( 'Footer Menu', 'trailhead' ),		// Secondary nav in footer
+	)
+);
+
+// The Social Links Menu
+function trailhead_social_links() {
+	wp_nav_menu(array(
+		'container'			=> 'false',				// Remove nav container
+		'menu_id'			=> 'social-links',		// Adding custom nav id
+		'menu_class'		=> 'menu',				// Adding custom nav class
+		'theme_location'	=> 'social-links',		// Where it's located in the theme
+		'depth'				=> 0,					// Limit the depth of the nav
+		'fallback_cb'		=> ''					// Fallback function
+	));
+} /* End Social Links Menu */
+
+// The Header Menu
+function trailhead_header_secondary_nav() {
+	wp_nav_menu(array(
+		'container'			=> false,						// Remove nav container
+		'menu_id'			=> 'header-secondary-nav',					// Adding custom nav id
+		'menu_class'		=> 'horizontal menu',	// Adding custom nav class
+		'items_wrap'		=> '<ul id="%1$s" class="%2$s">%3$s</ul>',
+		'theme_location'	=> 'header-nav',					// Where it's located in the theme
+		'depth'				=> 1,							// Limit the depth of the nav
+		'fallback_cb'		=> false,						// Fallback function (see below)
+	));
+}
+
+// The Header Menu
+function trailhead_header_nav() {
+	wp_nav_menu(array(
+		'container'			=> false,						// Remove nav container
+		'menu_id'			=> 'header-nav',					// Adding custom nav id
+		'menu_class'		=> 'horizontal menu dropdown',	// Adding custom nav class
+		'items_wrap'		=> '<ul id="%1$s" class="%2$s" data-dropdown-menu data-submenu-toggle="true" data-hover-delay="200" data-closing-time="200">%3$s</ul>',
+		'theme_location'	=> 'header-nav',					// Where it's located in the theme
+		'depth'				=> 5,							// Limit the depth of the nav
+		'fallback_cb'		=> false,						// Fallback function (see below)
+		'walker'			=> new Header_Menu_Walker(),
+	));
+}
+
+// Big thanks to Brett Mason (https://github.com/brettsmason) for the awesome walker
+class Header_Menu_Walker extends Walker_Nav_Menu {
+	function start_lvl(&$output, $depth = 0, $args = Array() ) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "\n$indent<ul class=\"menu\">\n";
+	}
+}
+
+// The Off Canvas Menu
+function trailhead_off_canvas_nav() {
+	wp_nav_menu(array(
+		'container'			=> false,							// Remove nav container
+		'menu_id'			=> 'offcanvas-nav',					// Adding custom nav id
+		'menu_class'		=> 'vertical menu drilldown',	// Adding custom nav class
+		'items_wrap'		=> '<ul id="%1$s" class="%2$s" data-drilldown>%3$s</ul>',
+		'theme_location'	=> 'offcanvas-nav',					// Where it's located in the theme
+		'depth'				=> 5,								// Limit the depth of the nav
+		'fallback_cb'		=> false,							// Fallback function (see below)
+		'walker'			=> new Off_Canvas_Menu_Walker(),
+	));
+}
+
+class Off_Canvas_Menu_Walker extends Walker_Nav_Menu {
+	function start_lvl(&$output, $depth = 0, $args = Array() ) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "\n$indent<ul class=\"vertical menu\">\n";
+	}
+}
+
+// The Off Canvas Utility Menu
+function trailhead_off_canvas_supplemental_nav() {
+	wp_nav_menu(array(
+		'container'			=> false,							// Remove nav container
+		'menu_id'			=> 'offcanvas-secondary-nav',					// Adding custom nav id
+		'menu_class'		=> 'menu accordion-menu',	// Adding custom nav class
+		'items_wrap'		=> '<ul id="%1$s" class="%2$s" data-accordion-menu>%3$s</ul>',
+		'theme_location'	=> 'offcanvas-supplemental-nav',					// Where it's located in the theme
+		'depth'				=> 5,								// Limit the depth of the nav
+		'fallback_cb'		=> false,							// Fallback function (see below)
+		'walker'			=> new Off_Canvas_Secondary_Menu_Walker()
+	));
+}
+
+// The Footer Menu
+function trailhead_footer_nav() {
+	wp_nav_menu(array(
+		'container'			=> false,							// Remove nav container
+		'menu_id'			=> 'footer-nav',					// Adding custom nav id
+		'menu_class'		=> 'menu',	// Adding custom nav class
+		'items_wrap'		=> '<ul id="%1$s" class="%2$s">%3$s</ul>',
+		'theme_location'	=> 'footer-nav',					// Where it's located in the theme
+		'depth'				=> 2,								// Limit the depth of the nav
+		'fallback_cb'		=> false,							// Fallback function (see below)
+		'walker'			=> new Footer_Menu_Walker()
+	));
+}
+
+class Footer_Menu_Walker extends Walker_Nav_Menu {
+	function start_lvl(&$output, $depth = 0, $args = Array() ) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "\n$indent<ul class=\"vertical menu\">\n";
+	}
+}
+
+
+// Header Fallback Menu
+function trailhead_main_nav_fallback() {
+	wp_page_menu( array(
+		'show_home'		=> true,
+		'menu_class'	=> '',		// Adding custom nav class
+		'include'		=> '',
+		'exclude'		=> '',
+		'echo'			=> true,
+		'link_before'	=> '',		// Before each link
+		'link_after'	=> ''		// After each link
+	));
+}
+
+// Footer Fallback Menu
+function trailhead_footer_links_fallback() {
+	/* You can put a default here if you like */
+}
+
+// Add Foundation active class to menu
+function required_active_nav_class( $classes, $item ) {
+	if ( $item->current == 1 || $item->current_item_ancestor == true ) {
+		$classes[] = 'active';
+	}
+	return $classes;
+}
+add_filter( 'nav_menu_css_class', 'required_active_nav_class', 10, 2 );
+
+
+// Add ACF Fields to Main Nav
+
+	function my_wp_nav_menu_objects( $items, $args ) {
+					
+		if ( $args->theme_location == 'social-links') {
+			
+			// loop
+			foreach( $items as &$item ) {
+				
+				// vars
+				$icon = get_field('icon', $item);
+				$size = 'full';						
+				// append icon
+				if( $icon ) {
+					
+					$item->title = '<span class="icon" aria-hidden="true"><img class="style-svg" src="' . $icon['url'] . '" alt="' . $icon['alt'] . '"></span><span class="show-for-sr"' . $item->title . '</span>';
+					
+				}
+				
+			}
+			
+			// return
+			return $items;		
+
+		} else {			
+			// loop
+			foreach( $items as &$item ) {}
+			return $items;	
+		}
+		
+	}
+	
+add_filter('wp_nav_menu_objects', 'my_wp_nav_menu_objects', 10, 2);
---
+++ template-parts/nav-offcanvas-topbar.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * The off-canvas menu uses the Off-Canvas Component
+ *
+ * For more info: https://jointswp.com/docs/off-canvas-menu/
+ */
+ $learn_more_cta = get_field('learn_more_cta', 'option');
+ $enroll_cta = get_field('enroll_cta', 'option');
+?>
+
+<div class="top-bar-wrap grid-container fluid">
+	
+	<?php if( wp_get_nav_menu_items(get_nav_menu_locations()['social-links']) || wp_get_nav_menu_items(get_nav_menu_locations()['header-secondary-nav']) ): ?>
+		<div class="pre-header-nav">
+			<div class="grid-container fluid">
+				<div class="grid-x grid-padding-x align-middle">
+					<?php if( wp_get_nav_menu_items(get_nav_menu_locations()['social-links'])):?>
+						<div class="cell shrink">
+							<?php trailhead_social_links();?>
+						</div>
+					<?php endif;?>
+					<?php if( wp_get_nav_menu_items(get_nav_menu_locations()['header-secondary-nav'])):?>
+						<div class="cell auto grid-x align-middle align-right">
+							<?php trailhead_header_secondary_nav();?>
+						</div>
+					<?php endif;?>
+				</div>
+			</div>
+		</div>
+	<?php endif;?>
+	
+	<div class="header-middle">
+		<div class="site-branding show-for-sr">
+			<?php
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$trailhead_description = get_bloginfo( 'description', 'display' );
+			if ( $trailhead_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $trailhead_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+		<div class="grid-container">
+			<div class="grid-x grid-padding-x align-bottom">
+				<?php 
+				$image = get_field('header_logo', 'option');
+				if( !empty( $image ) ): ?>
+				<div class="left cell small-12 tablet-shrink">
+					<ul class="menu">
+						<li class="logo"><a href="<?php echo home_url(); ?>">
+							<?php if( !empty( get_field('header_logo', 'option') ) ) {
+								$imgID = get_field('header_logo', 'option')['ID'];
+								$img_alt = trim( strip_tags( get_post_meta( $imgID, '_wp_attachment_image_alt', true ) ) );
+								$img = wp_get_attachment_image( $imgID, 'full', false, [ "class" => "", "alt"=>$img_alt] );
+								echo $img;
+							}?>
+						</a></li>
+					</ul>
+				</div>
+				<?php endif;?>
+				<?php if( !empty($learn_more_cta) || !empty($learn_more_cta) ):?>
+					<div class="right cell small-12 tablet-auto">
+						<div class="grid-x grid-padding-x">
+							<?php if( !empty($learn_more_cta) ):?>
+								<?php
+									$link = $learn_more_cta;
+									$link_url = $link['url'];
+									$link_title = $link['title'];
+									$link_target = $link['target'] ? $link['target'] : '_self';
+								?>
+								<div class="cell small-6">
+									<a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+								</div>
+							<?php endif;?>
+							<?php if( !empty($enroll_cta) ):?>
+								<?php
+									$link = $learn_more_cta;
+									$link_url = $link['url'];
+									$link_title = $link['title'];
+									$link_target = $link['target'] ? $link['target'] : '_self';
+								?>
+								<div class="cell small-6">
+									<a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+								</div>
+							<?php endif;?>
+						</div>
+					</div>
+				<?php endif;?>
+			</div>
+		</div>
+	</div>
+
+	<?php if( wp_get_nav_menu_items(get_nav_menu_locations()['header-secondary-nav']) || wp_get_nav_menu_items(get_nav_menu_locations()['offcanvas-nav']) ):?>
+	<div class="top-bar" id="top-bar-menu">
+		<div class="grid-container">
+			<div class="grid-x grid-padding-x">
+				<?php if( wp_get_nav_menu_items(get_nav_menu_locations()['header-secondary-nav']) ):?>
+				<div class="top-bar-left">
+					<?php trailhead_header_nav();?>
+				</div>
+				<?php endif;?>
+				<?php if( wp_get_nav_menu_items(get_nav_menu_locations()['offcanvas-nav']) ):?>
+				<ul class="menu">
+					<!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
+					<li>
+						<a id="menu-toggle" data-toggle="off-canvas">
+							<span class="uppercase">Full Menu</span>
+							<svg xmlns="http://www.w3.org/2000/svg" width="17.334" height="16.788" viewBox="0 0 17.334 16.788">
+							  <path id="Path_457" data-name="Path 457" d="M-3997.248-7.817v-2.8h11.581v2.8Zm-2.285-7v-2.8h13.866v2.8Zm-3.467-7v-2.8h17.334v2.8Z" transform="translate(4003 24.605)" fill="#272727"/>
+							</svg>
+						</a>
+					</li>
+				</ul>
+				<?php endif;?>
+			</div>
+		</div>
+	</div>
+	<?php endif;?>
+	
+</div>
