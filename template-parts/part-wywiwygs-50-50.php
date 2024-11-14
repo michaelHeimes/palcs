dae8d97215065f6db3a056a584ec5da4e1d0773c
Michael Heimes <michael@cairndigitalmedia.com>
2024-11-14 14:44:11 -0500

    Removed heading from 50/50 part

--- template-parts/part-wywiwygs-50-50.php
+++ template-parts/part-wywiwygs-50-50.php
@@ -1,20 +1,11 @@
 <?php
 $is_intro = $args['is_intro'] ?? null;
 $wywiwygs_50_50 = $args['wywiwygs_50_50'] ?? null;
-$heading = $wywiwygs_50_50['heading'] ?? null;
 $left = $wywiwygs_50_50['left_wysiwyg'] ?? null;
 $right = $wywiwygs_50_50['right_wysiwyg'] ?? null;
 ?>
 <div class="wywiwygs-50-50<?php if( $is_intro == true ):?> intro<?php endif;?>">
 	<div class="grid-container">
-		<?php if( !empty($heading) ):?>
-			<div class="grid-x grid-padding-x align-center">
-				<div class="cell small-12 medium-6 large-5">
-					<h2 class="h1"><?=$heading;?></h2>
-				</div>
-				<div class="cell small-12 medium-6 large-5"></div>
-			</div>
-		<?php endif;?>
 		<div class="grid-x grid-padding-x align-center">
 			<?php if( !empty($left) ):?>
 				<div class="left cell small-12 medium-6 large-5">
